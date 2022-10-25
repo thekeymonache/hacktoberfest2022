1928e93439e16add656311413c56837dffa41db4
limajey <116623075+limajey@users.noreply.github.com>
2022-10-25 11:39:32 +0530

    Update CoinChange.cpp

--- CoinChange.cpp
+++ CoinChange.cpp
@@ -1,30 +1,44 @@
-
-#include<bits/stdc++.h>
+// Recursive C++ program for
+// coin change problem.
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
 
-int change_coin(int arr[],int m,int n,int j,int s){
-if(j>m||s>n)
-return 0;
-if(s==n){
-    
-return 1;
-}
-return(change_coin(arr,m,n,j,s+arr[j])+change_coin(arr,m,n,j+1,s));
-}
-int main(){
-     
-int m;//size of array
-cin>>m;
-int arr[m];
+// Returns the count of ways we can
+// sum coins[0...n-1] coins to get sum "sum"
+int count(int coins[], int n, int sum)
+{
 
-for(int i=0;i<m;i++){
-    cin>>arr[i];
+	// If sum is 0 then there is 1 solution
+	// (do not include any coin)
+	if (sum == 0)
+		return 1;
+
+	// If sum is less than 0 then no
+	// solution exists
+	if (sum < 0)
+		return 0;
+
+	// If there are no coins and sum
+	// is greater than 0, then no
+	// solution exist
+	if (n <= 0)
+		return 0;
+
+	// count is sum of solutions (i)
+	// including coins[n-1] (ii) excluding coins[n-1]
+	return count(coins, n - 1, sum)
+		+ count(coins, n, sum - coins[n - 1]);
 }
-int n;
-cin>>n;
-int j=0;
-int s=0;
-int r=change_coin(arr,m,n,j,s);
-cout<<r;
+
+// Driver code
+int main()
+{
+	int i, j;
+	int coins[] = { 1, 2, 3 };
+	int n = sizeof(coins) / sizeof(coins[0]);
+	int sum = 4;
+
+	cout << " " << count(coins, n, sum);
+
+	return 0;
 }
