f37d81c9bfddfc852355c9a08b4c0fa986ab9ddf
cexere <117029351+cexere@users.noreply.github.com>
2022-10-30 21:29:57 +0530

    Update EditDistance.cpp

--- EditDistance.cpp
+++ EditDistance.cpp
@@ -1,40 +1,46 @@
-
-// A Naive recursive C++ program to find minimum number
-// operations to convert str1 to str2
+// A Dynamic Programming based C++ program to find minimum
+// number operations to convert str1 to str2
 #include <bits/stdc++.h>
 using namespace std;
 
-// Utility function to find minimum of three numbers
+// Utility function to find the minimum of three numbers
 int min(int x, int y, int z) { return min(min(x, y), z); }
 
-int editDist(string str1, string str2, int m, int n)
+int editDistDP(string str1, string str2, int m, int n)
 {
-	// If first string is empty, the only option is to
-	// insert all characters of second string into first
-	if (m == 0)
-		return n;
+	// Create a table to store results of subproblems
+	int dp[m + 1][n + 1];
 
-	// If second string is empty, the only option is to
-	// remove all characters of first string
-	if (n == 0)
-		return m;
+	// Fill d[][] in bottom up manner
+	for (int i = 0; i <= m; i++) {
+		for (int j = 0; j <= n; j++) {
+			// If first string is empty, only option is to
+			// insert all characters of second string
+			if (i == 0)
+				dp[i][j] = j; // Min. operations = j
 
-	// If last characters of two strings are same, nothing
-	// much to do. Ignore last characters and get count for
-	// remaining strings.
-	if (str1[m - 1] == str2[n - 1])
-		return editDist(str1, str2, m - 1, n - 1);
+			// If second string is empty, only option is to
+			// remove all characters of second string
+			else if (j == 0)
+				dp[i][j] = i; // Min. operations = i
 
-	// If last characters are not same, consider all three
-	// operations on last character of first string,
-	// recursively compute minimum cost for all three
-	// operations and take minimum of three values.
-	return 1
-		+ min(editDist(str1, str2, m, n - 1), // Insert
-				editDist(str1, str2, m - 1, n), // Remove
-				editDist(str1, str2, m - 1,
-						n - 1) // Replace
-			);
+			// If last characters are same, ignore last char
+			// and recur for remaining string
+			else if (str1[i - 1] == str2[j - 1])
+				dp[i][j] = dp[i - 1][j - 1];
+
+			// If the last character is different, consider
+			// all possibilities and find the minimum
+			else
+				dp[i][j]
+					= 1
+					+ min(dp[i][j - 1], // Insert
+							dp[i - 1][j], // Remove
+							dp[i - 1][j - 1]); // Replace
+		}
+	}
+
+	return dp[m][n];
 }
 
 // Driver code
@@ -44,7 +50,7 @@ int main()
 	string str1 = "sunday";
 	string str2 = "saturday";
 
-	cout << editDist(str1, str2, str1.length(),
+	cout << editDistDP(str1, str2, str1.length(),
 					str2.length());
 
 	return 0;
