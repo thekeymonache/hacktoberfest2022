14a1cb21b2ecff684ea5b26cbd9bb5eaa667234c
cexere <117029351+cexere@users.noreply.github.com>
2022-10-30 21:32:31 +0530

    Update CoinChange.cpp

--- CoinChange.cpp
+++ CoinChange.cpp
@@ -1,44 +1,49 @@
-// Recursive C++ program for
-// coin change problem.
+// C++ program for coin change problem
+
 #include <bits/stdc++.h>
+
 using namespace std;
 
-// Returns the count of ways we can
-// sum coins[0...n-1] coins to get sum "sum"
 int count(int coins[], int n, int sum)
 {
+	int i, j, x, y;
 
-	// If sum is 0 then there is 1 solution
-	// (do not include any coin)
-	if (sum == 0)
-		return 1;
+	// We need sum+1 rows as the table
+	// is constructed in bottom up
+	// manner using the base case 0
+	// value case (sum = 0)
+	int table[sum + 1][n];
 
-	// If sum is less than 0 then no
-	// solution exists
-	if (sum < 0)
-		return 0;
+	// Fill the entries for 0
+	// value case (sum = 0)
+	for (i = 0; i < n; i++)
+		table[0][i] = 1;
 
-	// If there are no coins and sum
-	// is greater than 0, then no
-	// solution exist
-	if (n <= 0)
-		return 0;
+	// Fill rest of the table entries
+	// in bottom up manner
+	for (i = 1; i < sum + 1; i++) {
+		for (j = 0; j < n; j++) {
+			// Count of solutions including coins[j]
+			x = (i - coins[j] >= 0) ? table[i - coins[j]][j]
+									: 0;
 
-	// count is sum of solutions (i)
-	// including coins[n-1] (ii) excluding coins[n-1]
-	return count(coins, n - 1, sum)
-		+ count(coins, n, sum - coins[n - 1]);
+			// Count of solutions excluding coins[j]
+			y = (j >= 1) ? table[i][j - 1] : 0;
+
+			// total count
+			table[i][j] = x + y;
+		}
+	}
+	return table[sum][n - 1];
 }
 
-// Driver code
+// Driver Code
 int main()
 {
-	int i, j;
 	int coins[] = { 1, 2, 3 };
 	int n = sizeof(coins) / sizeof(coins[0]);
 	int sum = 4;
-
-	cout << " " << count(coins, n, sum);
-
+	cout << count(coins, n, sum);
 	return 0;
 }
+
